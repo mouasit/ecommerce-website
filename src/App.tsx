53a6551ce29710bbe80e3296038a7cc3b6938c66
mouasit <mustaphaouassit60@gmail.com>
2024-04-05 14:42:20 +0000

    add prettier tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className='bg-gray-800'>hello</div>
+      <div className="m-2 flex bg-gray-800 p-5 pt-3">hello</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
