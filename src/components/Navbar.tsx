3a903344cff654d71deebad6840e68e52ff0a780
mouasit <mustaphaouassit60@gmail.com>
2024-04-11 03:08:31 +0000

    add responsive ul

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,58 @@
-import { BurgerMenuIcon, ShoppingCartIcon } from "./Icons";
+import React from "react";
+import {
+  ArrowDownIcon,
+  BurgerMenuIcon,
+  CloseIcon,
+  ShoppingCartIcon,
+} from "./Icons";
 import logo from "../assets/logo.svg";
 
 export default function Navbar() {
+  const [clickBurgerMenu, setClickBurgerMenu] = React.useState<boolean>(false);
   return (
     <nav className="mt-[1.25rem] flex items-center justify-between px-4">
       <span className="flex items-center gap-5">
-        <button>
+        <button
+          className="lg:hidden"
+          onClick={() => {
+            setClickBurgerMenu(true);
+          }}
+        >
           <BurgerMenuIcon className="h-7 w-7 fill-bluePrimary" />
         </button>
         <button>
           <img src={logo} alt="logo" className="w-[8.5rem]" />
         </button>
       </span>
+      <ul
+        className={`fixed left-0 top-0 ${!clickBurgerMenu ? "hidden" : ""} h-full w-full items-center gap-20 space-y-9 bg-white p-4 lg:relative lg:flex lg:w-auto lg:space-y-0 lg:p-0`}
+      >
+        <li className="flex justify-end lg:hidden">
+          <button
+            className="rounded-full"
+            onClick={() => {
+              setClickBurgerMenu(false);
+            }}
+          >
+            <CloseIcon className="h-[.9rem] w-[.9rem] fill-bluePrimary" />
+          </button>
+        </li>
+        <li>
+          <button className="flex items-center gap-2 text-bluePrimary">
+            All products
+            <ArrowDownIcon className="h-3 w-3 fill-yellowPrimary" />
+          </button>
+        </li>
+        <li>
+          <button className="text-bluePrimary">Electronic</button>
+        </li>
+        <li>
+          <button className="text-bluePrimary">Audio & Video</button>
+        </li>
+        <li>
+          <button className="text-bluePrimary">Furniture</button>
+        </li>
+      </ul>
       <button className="flex items-start gap-[.2rem]">
         <ShoppingCartIcon className="h-[2.3rem] w-[2.3rem] fill-bluePrimary" />
         <span className="flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-full bg-yellowPrimary font-bold text-bluePrimary">
