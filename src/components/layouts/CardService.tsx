1d420b030db2fc74876162c00bdcb3e90dac4022
mouasit <mustaphaouassit60@gmail.com>
2024-04-19 16:55:03 +0100

    fix gap between cards

--- src/components/layouts/CardService.tsx
+++ src/components/layouts/CardService.tsx
@@ -1,12 +1,20 @@
 import { TrackIcon } from "../Icons";
 
-export default function CardService() {
+type ServiceInfo = {
+  icon: JSX.Element;
+  title: string;
+  desc: string;
+};
+
+export default function CardService({ icon, title, desc }: ServiceInfo) {
   return (
-    <div>
-      <TrackIcon />
-      <div>
-        <span>Free shipping</span>
-        <span>When you spend 30 MAD or more</span>
+    <div className="flex flex-col items-center gap-2 lg:flex-row lg:gap-4">
+      {icon}
+      <div className="flex flex-col items-center lg:items-start">
+        <span className="text-[1.1rem] font-semibold text-bluePrimary">
+          {title}
+        </span>
+        <span className="text-xs font-light text-grayPrimary">{desc}</span>
       </div>
     </div>
   );
