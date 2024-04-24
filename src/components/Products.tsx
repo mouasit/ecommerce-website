00904c606619fe032122c47569899ef08bdb5baa
mouasit <mustaphaouassit60@gmail.com>
2024-04-24 10:35:54 +0100

    fix justify content if it more than two chields

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,7 +2,7 @@ import Product from "./layouts/Product";
 
 export default function Products() {
   return (
-    <section className="mt-[5.7rem] flex flex-col items-center gap-16 px-4 sm:flex-row sm:flex-wrap sm:justify-between sm:gap-x-0 sm:px-[8vw] md:gap-x-0 md:px-4 lg:gap-x-[3rem] lg:px-4 2xl:px-0">
+    <section className="cards mt-[5.7rem] flex flex-col items-center gap-16 px-4 sm:flex-row sm:flex-wrap sm:justify-between sm:gap-x-0 sm:px-[8vw] md:gap-x-0 md:px-4 lg:gap-x-[3rem] lg:px-4 2xl:px-0">
       <Product />
       <Product />
       <Product />
