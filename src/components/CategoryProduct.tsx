373e3c3a9fbb6619dda49a070b9d65c53d73a5cc
mouasit <mustaphaouassit60@gmail.com>
2024-04-29 12:09:32 +0100

    add product content

--- src/components/CategoryProduct.tsx
+++ src/components/CategoryProduct.tsx
@@ -1,5 +1,9 @@
-import redmi from "../assets/products/redmi.png";
 import Product from "./layouts/Product";
+import redmi from "../assets/products/redmi.png";
+import airpods from "../assets/products/airpods.png";
+import ipad from "../assets/products/ipad.png";
+import headPhones from "../assets/products/headphones-black.png";
+import smartWatch from "../assets/products/smart-watch-red.png";
 
 export default function CategoryProduct() {
   return (
@@ -9,7 +13,13 @@ export default function CategoryProduct() {
       </h1>
       <div className="mt-[4rem] flex flex-col items-center gap-16">
         <Product name="Xiaomi redmi R..." price="1 500" imageProduct={redmi} />
-        <Product name="Xiaomi redmi R..." price="1 500" imageProduct={redmi} />
+        <Product name="Apple airpods 3" price="300" imageProduct={airpods} />
+        <Product
+          name="Surface Headpho..."
+          price="1 500"
+          imageProduct={headPhones}
+        />
+        <Product name="Smart watch red" price="300" imageProduct={smartWatch} />
       </div>
     </section>
   );
