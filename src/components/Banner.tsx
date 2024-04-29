ccc1715361cb43618d4c4286cfdce41855dbb8f1
mouasit <mustaphaouassit60@gmail.com>
2024-04-29 09:35:23 +0100

    fix size on banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,13 +3,13 @@ import bannerMobile from "../assets/banner-mobile.png";
 
 export default function Banner() {
   return (
-    <div className="mt-[8rem] px-4 2xl:px-0">
+    <div className="mt-[8rem] px-4">
       <img
         src={bannerDesktop}
         alt="banner"
-        className=" hidden w-full lg:block"
+        className=" hidden w-full md:block"
       />
-      <img src={bannerMobile} alt="banner" className="w-full lg:hidden" />
+      <img src={bannerMobile} alt="banner" className="w-full md:hidden" />
     </div>
   );
 }
