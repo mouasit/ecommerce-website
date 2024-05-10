f54dd71007906408e70313aa54b1e2587e8299f9
mouasit <mustaphaouassit60@gmail.com>
2024-05-10 12:20:30 +0100

    remove import slider

--- src/components/pages/Product.tsx
+++ src/components/pages/Product.tsx
@@ -1,6 +1,5 @@
 import ProductDetails from "../layouts/ProductDetails";
 import ProductTabs from "../layouts/ProductTabs";
-import ProductsCardsSlider from "../layouts/ProductsCardsSlider";
 import RelatedProducts from "../layouts/RelatedProducts";
 
 export default function Product() {
