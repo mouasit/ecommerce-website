369a3f9eb2512641c4034d51276970d5f9e5bd30
mouasit <mustaphaouassit60@gmail.com>
2024-05-10 12:31:21 +0100

    change spacing

--- src/components/layouts/RelatedProducts.tsx
+++ src/components/layouts/RelatedProducts.tsx
@@ -2,11 +2,11 @@ import ProductsCardsSlider from "./ProductsCardsSlider";
 
 export default function RelatedProducts() {
   return (
-    <div className="mt-[5.7rem] px-4 2xlg:px-0">
+    <div className="mt-[8rem] px-4 2xlg:px-0">
       <div className="text-center text-[1.9rem] font-semibold capitalize text-bluePrimary">
         related products
       </div>
-      <div className="mt-[5.7rem]">
+      <div className="mt-[4rem]">
         <ProductsCardsSlider />
       </div>
     </div>
