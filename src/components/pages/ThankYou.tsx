68b3320d008b778469424b04b232515c6a0b3c0a
mouasit <mustaphaouassit60@gmail.com>
2024-07-08 21:56:48 +0100

    feat: add header thank you page

--- src/components/pages/ThankYou.tsx
+++ src/components/pages/ThankYou.tsx
@@ -1,10 +1,26 @@
 import { useContext, useEffect } from "react";
 import { ShoppingCartContext } from "../../App";
+import { CheckGradientIcon } from "../layouts/Icons";
 
 export default function ThankYou() {
   const shoppingCartContext = useContext(ShoppingCartContext);
   useEffect(() => {
     shoppingCartContext.setShoppingCart([]);
   }, []);
-  return <div>thank you</div>;
+  return (
+    <div className="mt-[5.7rem] md:px-4 lg:px-0">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <CheckGradientIcon className="h-[6.5rem] w-[6.5rem]" />
+        <h1 className="text-[2rem] font-bold capitalize text-bluePrimary">
+          thank you
+        </h1>
+        <div className="flex flex-col items-center justify-center gap-1 font-light text-grayPrimary">
+          <span>Your order has been received</span>
+          <span>
+            We will notify you by email when your order has been shipped.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
