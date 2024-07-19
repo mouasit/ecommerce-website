91ff681857730c473c4b0efcd0f2bc73d7cc2654
mouasit <mustaphaouassit60@gmail.com>
2024-07-19 23:55:15 +0100

    fix: display additional information

--- src/components/layouts/ProductTabs.tsx
+++ src/components/layouts/ProductTabs.tsx
@@ -1,3 +1,4 @@
+import Information from "./Information";
 import { Tab, TabContent, Tabs, TabsList, TabsPanels } from "./Tabs";
 
 export default function ProductTabs() {
@@ -8,7 +9,7 @@ export default function ProductTabs() {
         <Tab>Additional information</Tab>
       </TabsList>
       <TabsPanels className="text-sm font-light text-bluePrimary">
-        <TabContent>
+        <TabContent className="text-[.9rem] leading-[1.8rem]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
@@ -17,14 +18,11 @@ export default function ProductTabs() {
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing.
         </TabContent>
-        <TabContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing.
+        <TabContent className="flex flex-col gap-6">
+          <Information numberOfInformation="01" />
+          <Information numberOfInformation="02" />
+          <Information numberOfInformation="03" />
+          <Information numberOfInformation="04" />
         </TabContent>
       </TabsPanels>
     </Tabs>
